b7b518b2318a87b09bcd9991b7ef752681910361
ilianapapadimitriou <ilianapapadimitirou.ceid@gmail.com>
2020-07-14 12:35:28 +0300

    Fix property name

---
+++ HelloGit/src/com/company/Person.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Person {
+
+    private String Firstname;
+
+
+    public void setFirstname (String firstname) {
+        Firstname = firstname;
+    }
+    public String getFirstname(){
+        return Firstname;
+    }
+}
